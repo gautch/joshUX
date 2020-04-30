0157cd30e1f238bbd037ecfea68d406754746b19
Josh <gautch@gmail.com>
2020-04-30 11:26:44 -0500

    lets try again
    trying again

--- JS/main.js
+++ JS/main.js
@@ -1,221 +1,146 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2015, Codrops
- * http://www.codrops.com
- */
-;(function(window) {
+let navigation = {
+  // Variables
+  $navTrigger: document.querySelector('.nav__trigger'),
+  $nav: document.querySelector('.nav'),
+  $navItems: document.querySelectorAll('.nav__item a'),
+  $main: document.querySelector('.main'),
+  transitionEnd: 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+  isOpeningNav: false,
 
-	'use strict';
+  init() {
+    let self = this;
 
-	var support = { transitions: Modernizr.csstransitions },
-		// transition end event name
-		transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		onEndTransition = function( el, callback ) {
-			var onEndCallbackFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.target != this ) return;
-					this.removeEventListener( transEndEventName, onEndCallbackFn );
-				}
-				if( callback && typeof callback === 'function' ) { callback.call(this); }
-			};
-			if( support.transitions ) {
-				el.addEventListener( transEndEventName, onEndCallbackFn );
-			}
-			else {
-				onEndCallbackFn();
-			}
-		},
-		// the pages wrapper
-		stack = document.querySelector('.pages-stack'),
-		// the page elements
-		pages = [].slice.call(stack.children),
-		// total number of page elements
-		pagesTotal = pages.length,
-		// index of current page
-		current = 0,
-		// menu button
-		menuCtrl = document.querySelector('button.menu-button'),
-		// the navigation wrapper
-		nav = document.querySelector('.pages-nav'),
-		// the menu nav items
-		navItems = [].slice.call(nav.querySelectorAll('.link--page')),
-		// check if menu is open
-		isMenuOpen = false;
+    // Reset overflow and height on load
+    self.$main.style.overflow = 'auto';
+    self.$main.style.height = 'auto';
 
-	function init() {
-		buildStack();
-		initEvents();
-	}
 
-	function buildStack() {
-		var stackPagesIdxs = getStackPagesIdxs();
+    // Handle scroll events
+    window.addEventListener('scroll', e => {
+      if (window.scrollY == 0 && self.isOpeningNav) {
+        self.isOpeningNav = false;
 
-		// set z-index, opacity, initial transforms to pages and add class page--inactive to all except the current one
-		for(var i = 0; i < pagesTotal; ++i) {
-			var page = pages[i],
-				posIdx = stackPagesIdxs.indexOf(i);
+        // Add a small delay
+        setTimeout(function () {
+          self.openNavigation();
+        }, 150);
+      }
+    });
 
-			if( current !== i ) {
-				classie.add(page, 'page--inactive');
+    // Handle .nav__trigger click event
+    self.$navTrigger.addEventListener('click', e => {
+      e.preventDefault();
 
-				if( posIdx !== -1 ) {
-					// visible pages in the stack
-					page.style.WebkitTransform = 'translate3d(0,100%,0)';
-					page.style.transform = 'translate3d(0,100%,0)';
-				}
-				else {
-					// invisible pages in the stack
-					page.style.WebkitTransform = 'translate3d(0,75%,-300px)';
-					page.style.transform = 'translate3d(0,75%,-300px)';		
-				}
-			}
-			else {
-				classie.remove(page, 'page--inactive');
-			}
+      if (!self.$navTrigger.classList.contains('is-active')) {
+        if (window.scrollY !== 0) {
+          // Scroll to top
+          window.scroll({ top: 0, left: 0, behavior: 'smooth' });
 
-			page.style.zIndex = i < current ? parseInt(current - i) : parseInt(pagesTotal + current - i);
-			
-			if( posIdx !== -1 ) {
-				page.style.opacity = parseFloat(1 - 0.1 * posIdx);
-			}
-			else {
-				page.style.opacity = 0;
-			}
-		}
-	}
+          // Enable opening nav
+          self.isOpeningNav = true;
+        } else {
+          self.openNavigation();
+        }
+      } else {
+        self.closeNavigation();
+      }
+    });
 
-	// event binding
-	function initEvents() {
-		// menu button click
-		menuCtrl.addEventListener('click', toggleMenu);
+    // Handle .nav__item click events
+    self.$navItems.forEach(navLink => {
+      navLink.addEventListener('click', function (e) {
+        e.preventDefault();
 
-		// navigation menu clicks
-		navItems.forEach(function(item) {
-			// which page to open?
-			var pageid = item.getAttribute('href').slice(1);
-			item.addEventListener('click', function(ev) {
-				ev.preventDefault();
-				openPage(pageid);
-			});
-		});
+        // Remove is-active from all .nav__items
+        self.$navItems.forEach(el => {
+          el.classList.remove('is-active');
+        });
 
-		// clicking on a page when the menu is open triggers the menu to close again and open the clicked page
-		pages.forEach(function(page) {
-			var pageid = page.getAttribute('id');
-			page.addEventListener('click', function(ev) {
-				if( isMenuOpen ) {
-					ev.preventDefault();
-					openPage(pageid);
-				}
-			});
-		});
+        // Ad is-active to clicked .nav__item
+        this.classList.add('is-active');
 
-		// keyboard navigation events
-		document.addEventListener( 'keydown', function( ev ) {
-			if( !isMenuOpen ) return; 
-			var keyCode = ev.keyCode || ev.which;
-			if( keyCode === 27 ) {
-				closeMenu();
-			}
-		} );
-	}
+        // Transition the page
+        self.transitionPage();
+      });
+    });
+  },
 
-	// toggle menu fn
-	function toggleMenu() {
-		if( isMenuOpen ) {
-			closeMenu();
-		}
-		else {
-			openMenu();
-			isMenuOpen = true;
-		}
-	}
+  openNavigation() {
+    let self = this;
 
-	// opens the menu
-	function openMenu() {
-		// toggle the menu button
-		classie.add(menuCtrl, 'menu-button--open')
-		// stack gets the class "pages-stack--open" to add the transitions
-		classie.add(stack, 'pages-stack--open');
-		// reveal the menu
-		classie.add(nav, 'pages-nav--open');
+    // .nav--trigger active
+    self.$navTrigger.classList.add('is-active');
 
-		// now set the page transforms
-		var stackPagesIdxs = getStackPagesIdxs();
-		for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
-			var page = pages[stackPagesIdxs[i]];
-			page.style.WebkitTransform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)'; // -200px, -230px, -260px
-			page.style.transform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)';
-		}
-	}
+    // body froze
+    document.body.classList.add('is-froze');
 
-	// closes the menu
-	function closeMenu() {
-		// same as opening the current page again
-		openPage();
-	}
+    // Remove old inline styles
+    if (self.$main.style.removeProperty) {
+      self.$main.style.removeProperty('overflow');
+      self.$main.style.removeProperty('height');
+    } else {
+      self.$main.style.removeAttribute('overflow');
+      self.$main.style.removeAttribute('height');
+    }
 
-	// opens a page
-	function openPage(id) {
-		var futurePage = id ? document.getElementById(id) : pages[current],
-			futureCurrent = pages.indexOf(futurePage),
-			stackPagesIdxs = getStackPagesIdxs(futureCurrent);
+    // .main active
+    self.$main.classList.add('is-active');
+  },
 
-		// set transforms for the new current page
-		futurePage.style.WebkitTransform = 'translate3d(0, 0, 0)';
-		futurePage.style.transform = 'translate3d(0, 0, 0)';
-		futurePage.style.opacity = 1;
+  closeNavigation() {
+    let self = this;
 
-		// set transforms for the other items in the stack
-		for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
-			var page = pages[stackPagesIdxs[i]];
-			page.style.WebkitTransform = 'translate3d(0,100%,0)';
-			page.style.transform = 'translate3d(0,100%,0)';
-		}
+    // .nav--trigger inactive
+    self.$navTrigger.classList.remove('is-active');
 
-		// set current
-		if( id ) {
-			current = futureCurrent;
-		}
-		
-		// close menu..
-		classie.remove(menuCtrl, 'menu-button--open');
-		classie.remove(nav, 'pages-nav--open');
-		onEndTransition(futurePage, function() {
-			classie.remove(stack, 'pages-stack--open');
-			// reorganize stack
-			buildStack();
-			isMenuOpen = false;
-		});
-	}
+    // .main inactive
+    self.$main.classList.remove('is-active');
+    self.$main.addEventListener('transitionend', e => {
+      if (e.propertyName == 'transform' && !self.$navTrigger.classList.contains('is-active')) {
+        // Reset overflow and height
+        self.$main.style.overflow = 'auto';
+        self.$main.style.height = 'auto';
 
-	// gets the current stack pages indexes. If any of them is the excludePage then this one is not part of the returned array
-	function getStackPagesIdxs(excludePageIdx) {
-		var nextStackPageIdx = current + 1 < pagesTotal ? current + 1 : 0,
-			nextStackPageIdx_2 = current + 2 < pagesTotal ? current + 2 : 1,
-			idxs = [],
+        // body unfroze
+        document.body.classList.remove('is-froze');
+      }
+    });
 
-			excludeIdx = excludePageIdx || -1;
+    // no-csstransitions fallback
+    if (document.documentElement.classList.contains('no-csstransitions')) {
+      // .main inactive
+      self.$main.classList.remove('is-active');
 
-		if( excludePageIdx != current ) {
-			idxs.push(current);
-		}
-		if( excludePageIdx != nextStackPageIdx ) {
-			idxs.push(nextStackPageIdx);
-		}
-		if( excludePageIdx != nextStackPageIdx_2 ) {
-			idxs.push(nextStackPageIdx_2);
-		}
+      // body unfroze
+      document.body.classList.remove('is-froze');
+    }
+  },
 
-		return idxs;
-	}
+  transitionPage() {
+    let self = this;
 
-	init();
+    // .main transitioning
+    self.$main.classList.add('is-transition-out');
+    self.$main.addEventListener('transitionend', e => {
+      if (e.propertyName == 'clip-path') {
+        if (self.$main.classList.contains('is-transition-in')) {
+          self.$main.classList.remove('is-transition-in');
+          self.$main.classList.remove('is-transition-out');
+          self.closeNavigation();
+        }
 
-})(window);
+        if (self.$main.classList.contains('is-transition-out')) {
+          self.$main.classList.remove('is-transition-out');
+
+          // Add new content to .main
+
+          setTimeout(function () {
+            self.$main.classList.add('is-transition-in');
+          }, 500);
+        }
+      }
+    });
+  } };
+
+
+navigation.init();
